77444ce5444a69303b8d5c7849d656b5960fe791
elleobrien <elleobrien11@gmail.com>
2020-10-12 12:57:33 -0700

    First commit

---
+++ eval.py
@@ -0,0 +1,36 @@
+from transformers import pipeline, AutoTokenizer
+import nlpaug.augmenter.char as nac
+import json
+
+# Load model 
+model = pipeline("sentiment-analysis",model="bert-base-uncased")
+
+# Define text perturbation
+aug = nac.KeyboardAug() # Insert realistic keystroke errors
+def typo(input):
+    output = aug.augment(input)
+    return(output)
+
+def eval_perturb(input_a,input_b):
+    output_a, output_b = model([input_a, input_b])
+    sq_error = (output_a["score"] - output_b["score"])**2
+    acc = output_a["label"] == output_b["label"]
+    return(sq_error,acc)
+
+
+# Read in our test dataset
+f = open("data/test_set.tsv")
+test_dataset = f.read().split("\t")[:-1]
+
+# Loop over all test examples and evaluate
+mse, total_acc = 0,0
+n = len(test_dataset)
+for sentence in test_dataset:
+    sq_error, acc = eval_perturb(sentence, typo(sentence))
+    mse += (1/n) * sq_error
+    total_acc += (1/n) * acc
+
+# Write results to file
+with open("perturbaton_test.json", 'w') as outfile:
+        json.dump({ "accuracy": total_acc, "mse":mse}, outfile)
+
---
+++ make_test_set.py
@@ -0,0 +1,20 @@
+import nltk
+import random
+
+f = open("data/raw.txt")
+corpus = f.read()
+
+# Split into sentences
+sent_list = nltk.tokenize.sent_tokenize(corpus)
+# Remove any sentences that are suspiciously short - say <= 15 characters
+clean_list = [s for s in sent_list if len(s) > 15]
+
+# Randomly select 1000 for testing
+random.seed(1)
+keep = random.sample(clean_list, 1000)
+
+# Write this subset to file
+with open('data/test_set.tsv', 'w') as f:
+    for item in keep:
+        # Remove any newlines in the body of the text to avoid confusion
+        f.write("%s\t" % item.strip())
