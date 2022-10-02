8c6f8e209dceab785d11d2d3ba1acb32dc400b12
mgovindasamy <govinm@gmail.com>
2022-10-01 21:59:04 -0700

    Update run_perturbation_test.py

--- run_perturbation_test.py
+++ run_perturbation_test.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 # Load model 
 tokenizer = AutoTokenizer.from_pretrained("textattack/albert-base-v2-SST-2")
-inference_model = AutoModelForSequenceClassification.from_pretrained("textattack/albert-base-v2-SST-2")
+inference_model = AutoModelForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
 model = pipeline("sentiment-analysis", model = inference_model,tokenizer=tokenizer)
 
 # Define text perturbation
